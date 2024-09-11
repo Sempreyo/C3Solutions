249fa7bc9caf1c9278d89a28151e864225e6abb3
Zhukov <azhukov@cnews.ru>
2024-09-11 18:04:19 +0300

    upd

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const chart = document.getElementById("pieDiag");
+    const chart1 = document.getElementById("diag1");
+    const chart2 = document.getElementById("diag2");
 
-    if (chart) {
-        new Chart(chart, {
+    if (chart1) {
+        new Chart(chart1, {
             plugins: [ChartDataLabels],
             type: "doughnut",
             options: {
@@ -44,6 +45,51 @@ document.addEventListener("DOMContentLoaded", () => {
                         "#9acedc",
                         "#489bd1",
                         "#acb9c9"
+                    ],
+                    hoverOffset: 4
+                }]
+            }
+        });
+    }
+
+    if (chart2) {
+        new Chart(chart2, {
+            plugins: [ChartDataLabels],
+            type: "doughnut",
+            options: {
+                rotation: 41,
+                cutout: 60,
+                plugins: {
+                    datalabels: {
+                        color: "black",
+                        font: {
+                            size: "13",
+                        },
+                        textAlign: "center",
+                        formatter: function (value, context) {
+                            return value + "%" + "\n" + context.chart.data.labels[context.dataIndex];
+                        }
+                    },
+                    legend: {
+                        display: false,
+                    },
+                    tooltip: {
+                        enabled: false
+                    }
+                }
+            },
+            data: {
+                labels: [
+                    "Да",
+                    "Не \n знаю",
+                    "Нет"
+                ],
+                datasets: [{
+                    data: [29, 7, 65],
+                    backgroundColor: [
+                        "#95d5de",
+                        "#b094c4",
+                        "#f9d098"
                     ],
                     hoverOffset: 4
                 }]
